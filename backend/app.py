e7683b8ea30c361f6f743bf0a7f48ddda1ba30a0
Mrkingggg <1529716150@qq.com>
2024-04-19 11:36:44 -0400

    docker

--- backend/app.py
+++ backend/app.py
@@ -339,4 +339,4 @@ def browse_patients():
 # @app.route()
 if __name__ == '__main__':
     # db.create_all()
-    app.run(host='0.0.0.0', port=5050, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
